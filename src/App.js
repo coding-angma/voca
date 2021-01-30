e85fe9859f9adab559551db4a092207de55a27d9
coding-angma <rroocckk2090@gmail.com>
2021-01-30 23:45:14 +0900

    lesson5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import Hello from "./component/Hello";
 import Welcome from "./component/Welcome";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className="App">
       <Hello />
-      <Welcome />
-      <Hello />
-      <Hello />
+      <div className={styles.box}>App</div>
     </div>
   );
 }
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,11 +1,20 @@
 import World from "./World";
+import styles from "./Hello.module.css";
 
 export default function Hello() {
   return (
     <div>
-      <h1>Hello</h1>
-      <World />
-      <World />
+      <h1
+        style={{
+          color: "#f00",
+          borderRight: "12px solid #000",
+          marginBottom: "50px",
+          opacity: 1,
+        }}
+      >
+        Hello
+      </h1>
+      <div className={styles.box}>Hello</div>
     </div>
   );
 }
