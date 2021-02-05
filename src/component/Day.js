7e75d6730c28ed4e669d7415893b9aa27311a793
coding-angma <rroocckk2090@gmail.com>
2021-02-06 02:25:36 +0900

    lesson11

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,5 +1,6 @@
 import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
+import Word from "./Word";
 
 export default function Day() {
   const { day } = useParams();
@@ -11,10 +12,7 @@ export default function Day() {
       <table>
         <tbody>
           {wordList.map(word => (
-            <tr key={word.id}>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
+            <Word word={word} key={word.id} />
           ))}
         </tbody>
       </table>
---
+++ src/component/Word.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function Word({ word }) {
+  const [isShow, setIsShow] = useState(false);
+  const [isDone, setIsDone] = useState(word.isDone);
+
+  function toggleShow() {
+    setIsShow(!isShow);
+  }
+
+  function toggleDone() {
+    setIsDone(!isDone);
+  }
+
+  return (
+    <tr className={isDone ? "off" : ""}>
+      <td>
+        <input type="checkbox" checked={isDone} onChange={toggleDone} />
+      </td>
+      <td>{word.eng}</td>
+      <td>{isShow && word.kor}</td>
+      <td>
+        <button onClick={toggleShow}>뜻 {isShow ? "숨기기" : "보기"}</button>
+        <button className="btn_del">삭제</button>
+      </td>
+    </tr>
+  );
+}
