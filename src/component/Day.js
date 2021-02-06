9374c17d8c4b46e12a40e93f37de1d4b2ca2532a
coding-angma <rroocckk2090@gmail.com>
2021-02-06 18:34:24 +0900

    lesson12

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,17 +1,27 @@
-import dummy from "../db/data.json";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
 
 export default function Day() {
   const { day } = useParams();
-  const wordList = dummy.words.filter(word => word.day === Number(day));
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setWords(data);
+      });
+  }, [day]);
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map(word => (
+          {words.map(word => (
             <Word word={word} key={word.id} />
           ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import dummy from "../db/data.json";
 
 export default function DayList() {
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/days")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setDays(data);
+      });
+  }, []);
+
   return (
     <ul className="list_day">
-      {dummy.days.map(day => (
+      {days.map(day => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}>Day {day.day}</Link>
         </li>
