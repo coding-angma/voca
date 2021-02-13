254682a66d1df2f3d4e2f856d51751fe574ae2ba
coding-angma <rroocckk2090@gmail.com>
2021-02-14 01:00:15 +0900

    lesson14

--- src/component/Word.js
+++ src/component/Word.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-export default function Word({ word }) {
+export default function Word({ word: w }) {
+  const [word, setWord] = useState(w);
   const [isShow, setIsShow] = useState(false);
   const [isDone, setIsDone] = useState(word.isDone);
 
@@ -9,7 +10,36 @@ export default function Word({ word }) {
   }
 
   function toggleDone() {
-    setIsDone(!isDone);
+    fetch(`http://localhost:3001/words/${word.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...word,
+        isDone: !isDone,
+      }),
+    }).then(res => {
+      if (res.ok) {
+        setIsDone(!isDone);
+      }
+    });
+  }
+
+  function del() {
+    if (window.confirm("삭제 하시겠습니까?")) {
+      fetch(`http://localhost:3001/words/${word.id}`, {
+        method: "DELETE",
+      }).then(res => {
+        if (res.ok) {
+          setWord({ id: 0 });
+        }
+      });
+    }
+  }
+
+  if (word.id === 0) {
+    return null;
   }
 
   return (
@@ -21,8 +51,15 @@ export default function Word({ word }) {
       <td>{isShow && word.kor}</td>
       <td>
         <button onClick={toggleShow}>뜻 {isShow ? "숨기기" : "보기"}</button>
-        <button className="btn_del">삭제</button>
+        <button onClick={del} className="btn_del">
+          삭제
+        </button>
       </td>
     </tr>
   );
 }
+
+// Create - POST
+// Read - GET
+// Update - PUT
+// Delete - DELETE
